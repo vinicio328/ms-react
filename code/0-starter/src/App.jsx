cec5271469c08ae907527ac5422ccfa78e8b51b6
Vinicio <viniciojoj@gmail.com>
2022-01-11 21:25:38 -0600

    Creating a component

--- code/0-starter/src/App.jsx
+++ code/0-starter/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import RecipeTitle from './RecipeTitle';
 
 function App()
 {
     return (
         <article>
             <h1>Recipe Manager</h1>
+            <RecipeTitle></RecipeTitle>
         </article>
     )
 }
